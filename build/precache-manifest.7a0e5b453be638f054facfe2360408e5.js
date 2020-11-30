419590b1c82a6e1cb39ee4814ee737b48f9a47df
MFarid6363 <faridmurvatov63@gmail.com>
2020-11-30 10:08:31 +0100

    third commit

---
+++ build/precache-manifest.7a0e5b453be638f054facfe2360408e5.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = [
+  {
+    "revision": "6930a364aacc00100ba0",
+    "url": "/static/css/main.0b4565a6.chunk.css"
+  },
+  {
+    "revision": "6930a364aacc00100ba0",
+    "url": "/static/js/main.45fbc029.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "06ac1f12645b2647752f",
+    "url": "/static/js/2.5d6fc75e.chunk.js"
+  },
+  {
+    "revision": "9d923f822b2ddaef73b6c18ba74d61e3",
+    "url": "/static/media/logo.9d923f82.jpg"
+  },
+  {
+    "revision": "badecba09fb2e3a1f73eb40a3436db39",
+    "url": "/static/media/Electronics.badecba0.jpg"
+  },
+  {
+    "revision": "ccdab7775e3fdf448234ab108aa0c6ad",
+    "url": "/static/media/Language.ccdab777.jpg"
+  },
+  {
+    "revision": "2345a556d0799a195e86fc53bf07bac5",
+    "url": "/static/media/Math.2345a556.jpg"
+  },
+  {
+    "revision": "09eb96bfd7c28c3d6e4e24ee2b2b6705",
+    "url": "/static/media/React.09eb96bf.jpg"
+  },
+  {
+    "revision": "ab97507283e273f05bb7917d540f3aa0",
+    "url": "/static/media/Sport.ab975072.jpg"
+  },
+  {
+    "revision": "0430ea087444961710f0df27e2e66131",
+    "url": "/static/media/Main.0430ea08.jpg"
+  },
+  {
+    "revision": "29b7d3c1fb91aa5f27eb4e3d54204bd4",
+    "url": "/static/media/registerback.29b7d3c1.jpg"
+  },
+  {
+    "revision": "a4bb55e9c0a414df34de6a9d89b90b51",
+    "url": "/index.html"
+  }
+];
